2ba5ecd779b9f283fc0c2dd1770c2ac7f28d1eac
Katie Lewis <kmlewis@mit.edu>
2021-06-02 14:32:06 -0700

    fixed demo

--- static_files/resources/try_on.js
+++ static_files/resources/try_on.js
@@ -1,39 +1,15 @@
-function display(src_name, id_name){
+function display(src_name){
 	output = document.getElementById("interpolatedResult")
 	model_image = document.getElementById("modelBigImage")
+	output.src = src_name
 
-
-	if(src_name == "static_files/resources/target/43bX93WjTb8Z4w2aHVTujo_o4XzSw3Sqkr9hvNGt8tTfb_seed1332.png"){
+	if(src_name == "model0"){
 		output.classList.add("hidden")
-		model_image.src = src_name
 		model_image.classList.remove("hidden")
-		console.log('model image',model_image.src)
-
 	}
 	else{
 		model_image.classList.add("hidden")
 		output.classList.remove("hidden")
-	}
-
-	if(src_name == "static_files/resources/target/5spkYqhxaEUq7QXXZdtdGJ_cHbd6QdQkHPFsat9PUcsq6_seed12431.png"){
-		output.classList.add("hidden")
-		model_image.src = src_name
-		model_image.classList.remove("hidden")
-		console.log('model image',model_image.src)
-
-	}
-	else{
-		model_image.classList.add("hidden")
-		output.classList.remove("hidden")
-	}
-
-	if(model_image.src === "http://localhost:8000/static_files/resources/target/43bX93WjTb8Z4w2aHVTujo_o4XzSw3Sqkr9hvNGt8tTfb_seed1332.png"){
-		output.src = src_name
-		console.log('src',output.src)
-	}
-	else{
-		output.src = id_name
-		console.log('id',output.src)
 	}
 
 }
